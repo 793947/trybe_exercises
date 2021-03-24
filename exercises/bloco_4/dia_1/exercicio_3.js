864fb65f2a4b94fd1806654855a029a76517cebe
GustavoReginato <gureginatopd@gmail.com>
2021-03-24 15:16:21 -0300

    adiciona exercicio3

---
+++ exercises/bloco_4/dia_1/exercicio_3.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 20;
+let c = 15;
+
+if (a > b && c) {
+  console.log("a é maior");
+} 
+else if (b > a && c){
+  console.log("b é maior")
+} else 
+  console.log("c é o maior"); 
+
+
