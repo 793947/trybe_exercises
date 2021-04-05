9c63f0d767fceb3679d9caeb4bae4f2e7af7baed
GustavoReginato <gureginatopd@gmail.com>
2021-04-05 16:25:47 -0300

    mudar cor do quadrado para verde

--- exercises/bloco_5/dia_1/script.js
+++ exercises/bloco_5/dia_1/script.js
@@ -1,1 +1,7 @@
 document.getElementById('text1').innerHTML="Daqui 2 anos me vejo trabalhando na área da tecnologia, em uma empresa que me agrade e esteja de acordo com meus principios";
+
+function alteraCor(elementos, cor){
+  document.querySelector(elementos).style.backgroundColor = cor;
+};
+
+alteraCor('.main-content', 'rgb(76,164,109)');
