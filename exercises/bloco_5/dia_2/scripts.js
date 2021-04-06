b92ada58ca7f17c45bbac2fc629c29ac1aca79c5
GustavoReginato <gureginatopd@gmail.com>
2021-04-06 18:46:52 -0300

    adiciona exercicio 2

--- exercises/bloco_5/dia_2/scripts.js
+++ exercises/bloco_5/dia_2/scripts.js
@@ -3,3 +3,8 @@ let meuH1 = document.createElement('h1');
 document.body.appendChild(meuH1);
 meuH1.innerHTML = 'Exercício 5.2 - JavaScript DOM';
 meuH1.style.backgroundColor = 'gray';
+
+//exercicio numero 2;
+let minhaDiv = document.createElement('div');
+minhaDiv.className = 'main-content';
+document.body.appendChild(minhaDiv);
